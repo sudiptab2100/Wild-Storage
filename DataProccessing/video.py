bd6f7d73a939518fbc479c5622bba7835cead31b
Sudipta Basak <sudiptabasak@pool-100-10-15-65.prvdri.fios.verizon.net>
2023-03-13 05:08:09 +0530

    video module added

---
+++ DataProccessing/video.py
@@ -0,0 +1,37 @@
+import cv2
+import json
+
+class FrameVideo:
+    def __init__(self, ftype=0):
+        self.ftype = ftype
+    
+    def __getMetadata(self, target_dir):
+        with open(f"{target_dir}metadata.json", "r") as f:
+            return json.load(f)
+    
+    def __frames(self, target_dir, n):
+        for i in range(n):
+            fi_path = f"{target_dir}{i}.png"
+            yield cv2.imread(fi_path, cv2.IMREAD_GRAYSCALE)
+    
+    def framesToVideo(self, target_dir, dest):
+        metadata = self.__getMetadata(target_dir)
+        n = metadata['frames']
+        out = cv2.VideoWriter(dest + 'op.avi', cv2.VideoWriter_fourcc(*'MPNG'), 15, (metadata['width'], metadata['height']), 0)
+        for frame in self.__frames(target_dir, n):
+            out.write(frame)
+        out.release()
+
+    def extractFrames(self, target, dest):
+        metadata = self.__getMetadata(target)
+        vidObj = cv2.VideoCapture(target + 'op.avi')
+    
+        count = 0
+        success = True
+        while success:
+            success, image = vidObj.read()
+
+            if success: 
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                cv2.imwrite(f"{dest}{count}.png", image)
+                count += 1
