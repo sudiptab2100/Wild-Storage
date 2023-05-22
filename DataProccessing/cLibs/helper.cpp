4df93e7cccf6926977d0891752195a99b59d0b65
Sudipta Basak <sudiptab2100@gmail.com>
2023-05-23 00:40:43 +0530

    C++ binding with pixelize module

---
+++ DataProccessing/cLibs/helper.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+string ppixelToBit(const int* array, const size_t size) {
+    string bits;
+
+    for (int i = 0; i < size; ++i) {
+        bits += to_string(static_cast<int>(round(array[i] / 255.0)) > 0.5 ? 1 : 0);
+    }
+
+    return bits;
+}
+
+extern "C" {
+    const char* c_pixelToBit(const int* array, const size_t size) { 
+        string bits = ppixelToBit(array, size); 
+        return bits.c_str();
+    }
+}
+
+// g++ -fPIC -shared DataProccessing/cLibs/helper.cpp -o DataProccessing/cLibs/c_lib.so
---
+++ DataProccessing/c_bind.py
@@ -0,0 +1,13 @@
+from ctypes import *
+
+c_lib = cdll.LoadLibrary('/Users/sudiptabasak/Desktop/Wild-Storage/DataProccessing/cLibs/c_lib.so')
+
+def c_pixelToBit(array):
+    size = len(array)
+    carray = (c_int * size)(*array)
+
+    c_lib.c_pixelToBit.argtypes = [POINTER(c_int), c_size_t]
+    c_lib.c_pixelToBit.restype = c_char_p
+
+    res = c_lib.c_pixelToBit(carray, size)
+    return res.decode('utf-8')
