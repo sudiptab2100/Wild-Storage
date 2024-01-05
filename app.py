e69f657091aa0830565dd2bee9443f3a326639d8
Sudipta Basak <sudiptab2100@gmail.com>
2024-01-05 11:09:53 +0530

    Encoding feature implemented

--- app.py
+++ app.py
@@ -20,6 +20,42 @@ def delete_dir(settings):
     if os.path.exists(settings['data_dir']):
         shutil.rmtree(settings['data_dir'])
 
+def encode_to_video(settings):
+    ip_dir = settings['ip_files']
+    op_dir = settings['op_generated']
+    height = settings['height']
+    width = settings['width']
+    exp = settings['expansion_factor']
+    OPz = settings['zip_name']
+
+    z = ZipModule()
+    b = Bitify()
+    f = FrameGenerator(height, width, exp)
+    v = FrameVideo()
+
+    tic = time.perf_counter()
+    z.zip(OPz, ip_dir, op_dir)
+    toc = time.perf_counter()
+    print(f"zip done: {toc - tic:0.4f} seconds")
+
+    tic = time.perf_counter()
+    fbits = b.fileToBits(f'{op_dir}{OPz}.zip')
+    toc = time.perf_counter()
+    print(f"bitify done: {toc - tic:0.4f} seconds")
+
+    tic = time.perf_counter()
+    img_array = f.storeFrames(fbits, op_dir)
+    toc = time.perf_counter()
+    print(f"frame generator done: {toc - tic:0.4f} seconds")
+
+    tic = time.perf_counter()
+    v.framesToVideo(op_dir, img_array)
+    toc = time.perf_counter()
+    print(f"frames to video done: {toc - tic:0.4f} seconds")
+
+def decode_from_video(settings):
+    pass
+
 questions = [
     {
         'type': 'rawlist',
@@ -54,6 +90,6 @@ while True:
             create_dir(settings)
             print('Done!')
     elif answer == 'Encode file to video':
-        pass
+        encode_to_video(settings)
     elif answer == 'Decode video to file':
-        pass
+        decode_from_video(settings)
